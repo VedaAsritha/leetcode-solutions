3b69f146eb93452de05971e71ff48f09bcd3cad8
VedaAsritha <vedaasritha9@gmail.com>
2026-09-24 15:40:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (11.83%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSameAfterReversals(int n) {
+        if(n==0) return true;
+        if(n%10==0)
+        return false;
+        return true;
+        
+        
+    }
+}
